693e549628c5da7eec6e4d03e196423282847c71
Renier <renier.trenuela@gmail.com>
2024-04-24 11:47:46 +0800

    Add infolist in client resource

--- app/Filament/Resources/ClientResource.php
+++ app/Filament/Resources/ClientResource.php
@@ -5,8 +5,17 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\ClientResource\Pages;
 use App\Filament\Resources\ClientResource\RelationManagers;
 use App\Models\Client;
+use Filament\Actions\ViewAction;
 use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Infolists\Components\Tabs;
+use Filament\Infolists\Components\Tabs\Tab;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Components\View;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
@@ -18,13 +27,29 @@ class ClientResource extends Resource
 {
     protected static ?string $model = Client::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-users';
 
     public static function form(Form $form): Form
     {
         return $form
+            ->columns(12)
             ->schema([
-                //
+                TextInput::make('first_name')->columnSpan(4),
+                TextInput::make('middle_name')->columnSpan(4),
+                TextInput::make('last_name')->columnSpan(4),
+                Select::make('gender')
+                    ->columnSpan(4)
+                    ->options([
+                        'male' => 'Male',
+                        'female' => 'Female'
+                    ]),
+                TextInput::make('email')->columnSpan(5),
+                TextInput::make('phone_no')->columnSpan(4),
+                TextInput::make('phone_no_other')->columnSpan(4),
+                Textarea::make('address')->columnSpan(12),
+                TextInput::make('city')->columnSpan(4),
+                TextInput::make('state')->columnSpan(4),
+                TextInput::make('country')->columnSpan(4),
             ]);
     }
 
@@ -41,6 +66,7 @@ class ClientResource extends Resource
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make()->modalHeading('View Client'),
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
@@ -64,5 +90,38 @@ class ClientResource extends Resource
             'create' => Pages\CreateClient::route('/create'),
             'edit' => Pages\EditClient::route('/{record}/edit'),
         ];
+    }
+
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Tabs::make('Tabs')
+                    ->tabs([
+                        Tab::make('Details')
+                            ->columns(12)
+                            ->schema([
+                                TextEntry::make('first_name')->columnSpan(4),
+                                TextEntry::make('middle_name')->columnSpan(4),
+                                TextEntry::make('last_name')->columnSpan(4),
+                                TextEntry::make('gender'),
+                                TextEntry::make('email')->columnSpan(8),
+                                TextEntry::make('phone_no')->columnSpan(4)->label('Primary Phone No.'),
+                                TextEntry::make('phone_no_other')->columnSpan(6)->label('Alternative Phone No.'),
+                                TextEntry::make('address')->columnSpan(12),
+                                TextEntry::make('city')->columnSpan(4),
+                                TextEntry::make('state')->columnSpan(4),
+                                TextEntry::make('country')->columnSpan(4),
+                            ]),
+                        Tab::make('Cases')
+                            ->schema([
+                                // ...
+                            ]),
+                        Tab::make('Account')
+                            ->schema([
+                                // ...
+                            ]),
+                    ])->columnSpanFull(),
+            ]);
     }
 }
